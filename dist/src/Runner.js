5ab1b9fd6252c5a5c8a12ce28ddeb27ea2df5dcb
catpleademoji <catpleademoji@gmail.com>
2025-04-01 20:40:10 +0000

    mend

--- dist/src/Runner.js
+++ dist/src/Runner.js
@@ -1,15 +1,16 @@
 export class DefaultRunner {
     start(callback) {
         this.callback = callback;
-        this.animationFrameId = requestAnimationFrame(this.update);
+        this.update = this.update.bind(this);
+        this.animationFrameId = window.requestAnimationFrame(this.update);
     }
     stop() {
         if (this.animationFrameId) {
-            cancelAnimationFrame(this.animationFrameId);
+            window.cancelAnimationFrame(this.animationFrameId);
         }
     }
     update(timestamp) {
         this.callback && this.callback(timestamp);
-        this.animationFrameId = requestAnimationFrame(this.update);
+        this.animationFrameId = window.requestAnimationFrame(this.update);
     }
 }
